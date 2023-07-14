613c727dee85102b90861302688c63ce9e5cad59
Prashant <prashantamoli2621@gmail.com>
2023-07-14 13:19:20 +0530

    proof of concept

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				{/* <Script
+					src="https://tally.so/widgets/embed.js"
+					strategy="afterInteractive"
+					onLoad={() => {
+						console.log('loaded', Tally);
+						Tally.loadEmbeds();
+					}}
+				/> */}
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
